4c53833fad442784d8e7136e0db21a55fe18e655
shahjan ali <shahjanali@MacBook-Pro.local>
2023-12-12 21:43:41 +0000

    first commit

---
+++ helloworldss.py
@@ -0,0 +1,16 @@
+#print("hello  world!")
+course = 'python for me'
+course = course.replace('for', '4')
+print(course)
+
+
+temp = 37
+
+if temp > 30:
+    print ('its a hot day')
+
+else:
+    print('its not a hot day')
+
+print('')
+print('Hi, aiza')
